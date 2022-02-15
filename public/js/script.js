2a13f5d262b86701668fb3eeedfce3017b484771
Jérémy Doniak <jeremy_doniak@hotmail.com>
2022-02-15 16:52:43 +0100

    intégration html et css

---
+++ public/js/script.js
@@ -0,0 +1,78 @@
+/****************** HEADER *******************/
+
+$('.achat_resume').hide(100);
+$('.achat_container').hide(100);
+
+$('#achat').click(() => {
+	$('.achat_container').stop();
+	$('.achat_resume').stop();
+	$('.achat_resume').css("visibility", "visible");
+	$('.achat_container').slideToggle(1300);
+	$('.achat_resume').fadeToggle(1300);
+});
+
+/*//////////////////////////////// PAGE - HOME ////////////////////////////////*/
+
+/****************** SECTION-1 INTRO *******************/
+
+$('.intro_container h1').animate({
+	left: 0
+},1500);
+
+
+$('.btnleft').animate({
+	bottom: '50px',
+    opacity: "100%",
+},1300);
+
+$('.btnright').animate({
+	bottom: '50px',
+    opacity: "100%",
+},1000);
+
+
+/****************** SECTION-2 CAROUSEL *******************/
+
+const items = document.querySelectorAll('.carousel_item');
+const nbSlide = items.length;
+const suivant = document.querySelector('.arrow_right');
+const precedent = document.querySelector('.arrow_left');
+let count = 0;
+
+setTimeout(autoplay, 3000);
+function autoplay(){
+	items[count].classList.remove('active');
+
+	if(count < nbSlide - 1){
+		count++;
+	} else {
+		count = 0;
+	}
+	setTimeout(autoplay, 3000);
+	items[count].classList.add('active');
+}
+
+function slideSuivant(){
+	items[count].classList.remove('active');
+
+	if(count < nbSlide - 1){
+		count++;
+	} else {
+		count = 0;
+	}
+	items[count].classList.add('active');
+}
+suivant.addEventListener('click', slideSuivant);
+
+function slidePrecedent(){
+	items[count].classList.remove('active');
+
+	if(count > 0){
+		count--;
+	} else {
+		count = nbSlide - 1;
+	}
+
+	items[count].classList.add('active');
+}
+precedent.addEventListener('click', slidePrecedent);
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,4 +15,61 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/home', name: 'produits')]
+    public function produits(): Response
+    {
+        return $this->render('home/produits.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/home', name: 'renovation')]
+    public function renovation(): Response
+    {
+        return $this->render('home/renovation.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/home', name: 'home#sec_about')]
+    public function homeSecAbout(): Response
+    {
+        return $this->render('home/home#sec_about.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/home', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('home/contact.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/home', name: 'achat')]
+    public function achat(): Response
+    {
+        return $this->render('home/achat.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/home', name: 'cgu_cgv')]
+    public function cguCgv(): Response
+    {
+        return $this->render('home/cgu_cgv.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/home', name: 'faq')]
+    public function faq(): Response
+    {
+        return $this->render('home/faq.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
 }
